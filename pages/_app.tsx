4236407b75f7fc28cae0addc2258b927fc5d965d
deger4beger <deger.begerrr@gmail.com>
2022-08-22 13:26:45 +0600

    rq setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
+import { Hydrate, QueryClientProvider } from "react-query"
+import { queryClient } from "../src"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
