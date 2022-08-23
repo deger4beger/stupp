038c8723d28be7eef5963049d45341fc0b552aa8
deger4beger <deger.begerrr@gmail.com>
2022-08-23 16:51:35 +0600

    tailwind setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from "next"
 
 const Home: NextPage = () => {
-  return (
-    <div>
 
+  return (
+    <div className="flex justify-center">
+      Hello
     </div>
   )
 }
